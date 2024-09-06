46a2d09e8ec5ebf300754da0e5f90b46fc8d0e61
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-09-06 09:46:30 +0530

    unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,4 +26,43 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular_project_july_2024_2');
   });
+
+  it('Should verify add functionality', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.add(2, 3)).toBe(5);
+    expect(app.add(2, -3)).toBe(-1);
+    expect(app.add(-2, 3)).toBe(1);
+  })
+  it('Should verify isEven functionality', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.isEven(2)).toBe('even');
+    expect(app.isEven(2)).not.toBe('odd');
+    expect(app.isEven(3)).toBe('odd');
+    expect(app.isEven(5)).not.toBe('even');
+  })
+  it('Should verify cars functionality', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.cars).not.toBeUndefined();
+    expect(app.cars).toBeDefined;
+    expect(app.cars.length).toBe(2);
+    expect(app.cars).toContain('tata');
+    app.addNewCar('maruti')
+    app.addNewCar('hundai')
+    expect(app.cars.length).not.toBe(2);
+    expect(app.cars.length).toBe(4);
+    expect(app.cars).toContain('maruti');
+    app.deletecar(2);
+    expect(app.cars.length).not.toBe(4);
+    expect(app.cars.length).toBe(3);
+    expect(app.cars).toContain('tata');
+    expect(app.cars).not.toContain('maruti');
+  })
+  it('should verify paranthesis logic', () => {
+    let app = new AppComponent();
+    expect(app.isValidParentheses('{[()]}')).toBe(true);
+    expect(app.isValidParentheses('{[()]$')).toBe(false);
+  })
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,51 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular_project_july_2024_2';
+
+  add(a: number, b: number) {
+    return a + b;
+  }
+
+  isEven(num: number) {
+    if (num % 2 == 0) {
+      return 'even';
+    } else {
+      return 'odd';
+    }
+  }
+
+  cars: string[] = ['tata', 'honda'];
+  addNewCar(car: string) {
+    this.cars.push(car);
+  }
+  deletecar(ind: number) {
+    this.cars.splice(ind, 1)
+  }
+
+  isValidParentheses(str: string) {
+    const stack: any = [];
+    const pairs: any = {
+      "(": ")",
+      "[": "]",
+      "{": "}",
+    };
+    for (let char of str) {
+      if (pairs[char]) {
+        stack.push(char);
+      } else if (
+        char === ")" ||
+        char === "]" ||
+        char === "}"
+      ) {
+        if (
+          pairs[stack.pop()] !==
+          char
+        ) {
+          return false;
+        }
+      }
+    }
+    return stack.length === 0;
+  }
+
 }
